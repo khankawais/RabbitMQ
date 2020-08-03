0608540c0dd5002b15d964dba839c959c9a559e3
Awais Khan <53908905+khankawais@users.noreply.github.com>
2020-08-03 18:31:51 +0500

    Update logger.py

--- logger.py
+++ logger.py
@@ -1,5 +1,5 @@
 import logging
 LOG_FORMAT="%(levelname)s %(asctime)s - %(message)s"
-logging.basicConfig(filename='consumer.log',level=logging.INFO,format=LOG_FORMAT)
+logging.basicConfig(filename='/var/log/consumer.log',level=logging.INFO,format=LOG_FORMAT)
 
 genlog=logging.getLogger()
